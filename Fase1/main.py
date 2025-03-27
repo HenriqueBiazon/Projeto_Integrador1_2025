461efc6d2db7dab0b74627aca907c247c1b7752c
João Pedro Panza Mainieri <joaopedro.mainieri@gmail.com>
2025-03-27 16:34:11 -0300

    Reformatação do main.py
    mudei os nomes das variáveis, adicionei comentários, corrigi erros de português e exclui comparações desnecessárias

--- Fase1/main.py
+++ Fase1/main.py
@@ -1,40 +1,49 @@
-D = str(input('Qual a data de hoje? '))
-A = int(input('Quanto de agua voce consumiu hoje(aproximadamente em litros)?: '))
-E = float(input('Quantos KWh de energia eletrica voce consumiu hoje? '))
-R = float(input('Quantos kg de residuos nao reciclaveis voce gerou hoje? '))
-RR = float(input('Qual sua porcentagem de residuos reciclaveis hoje? '))
-print('Qual meio de transporte voce usou hoje?(responda com sim ou nao) ')
-Tp = str(input('transporte publico: '))
-B = str(input('Bicicleta: '))
-C = str(input('caminhada: '))
-Ca = str(input('Carro: '))
-Ce = str(input('Carro eletrico: '))
-Cc = str(input('Carona compartilhada: '))
-if ((B == 'sim') or (Tp == 'sim') or (C == 'sim') or (Ce == 'sim')) and ((Ca == 'sim') or (Cc == 'sim')):
-    print ('Sustentabilidade moderada para meio de transporte')
-elif (B == 'sim') or (Tp == 'sim') or (C == 'sim') or (Ce == 'sim'):
-    print('Alta sustentabilidade para meio de transporte')
-elif (Ca == 'sim') or (Cc == 'sim'):
-    print('baixa sustentabilidade para meio de transporte')
-# verificacao da sustentabilidade da agua
-if A < 150:
-    print('alta sustentabilidade no consumo de agua diario')
-elif 150 <= A <= 200:
-    print('moderado sustentabilidade no consumo de agua diario')
-elif A > 200:
-    print('baixo sustentabilidade no consumo de agua diario')
-#sustentabilidade no consumo de energia
-if E < 5:
-    print('alta sustentabilidade no consumo de energia')
-elif 5 <= E <= 10:
-    print('moderado sustentabilidade no consumo de energia')
-elif E > 10:
-    print('baixo sustentabilidade no consumo de energia')
-#sustentabilidade na geracao de residuos reciclaveis
-if RR > 50:
-    print('alta sustentabilidade na geracao de residuos reciclaveis')
-elif 20 <= RR <= 50:
-    print('moderado sustentabilidade na geracao de residuos reciclaveis')
-elif RR < 20:
-    print('baixo sustentabilidade na geracao de residuos reciclaveis')
-    
+# Entrada da data
+data = str(input('Qual a data de hoje? '))
+
+# Input dos consumos
+consumo_agua = int(input('Quantos L de água você consumiu hoje?: '))
+consumo_energia = float(input('Quantos KWh de energia elétrica você consumiu hoje? '))
+geracao_residuos = float(input('Quantos KG de resíduos não recicláveis você gerou hoje? '))
+residuos_reciclados = float(input('Qual sua porcentagem de resíduos reciclados hoje? '))
+
+# Input dos transportes
+print('Qual meio de transporte você usou hoje? (responda com SIM ou NÃO)')
+transporte_publico = input('Transporte público: ').upper()
+bicicleta = input('Bicicleta: ').upper()
+caminhada = input('Caminhada: ').upper()
+carro = input('Carro: ').upper()
+carro_eletrico = input('Carro elétrico: ').upper()
+carona_compartilhada = input('Carona compartilhada: ').upper()
+
+# Verificação da sustentabilidade da água
+if consumo_agua < 150:
+    print('Alta sustentabilidade no consumo de água diário.')
+elif 150 <= consumo_agua <= 200:
+    print('Moderada sustentabilidade no consumo de água diário.')
+else:
+    print('Baixa sustentabilidade no consumo de água diário.')
+
+# Sustentabilidade no consumo de energia
+if consumo_energia < 5:
+    print('Alta sustentabilidade no consumo de energia.')
+elif 5 <= consumo_energia <= 10:
+    print('Moderada sustentabilidade no consumo de energia.')
+else:
+    print('Baixa sustentabilidade no consumo de energia.')
+
+# Sustentabilidade na geração de resíduos recicláveis
+if residuos_reciclados > 50:
+    print('Alta sustentabilidade na geração de resíduos recicláveis.')
+elif 20 <= residuos_reciclados <= 50:
+    print('Moderada sustentabilidade na geração de resíduos recicláveis.')
+else:
+    print('Baixa sustentabilidade na geração de resíduos recicláveis.')
+
+# Sustentabilidade de meio de transporte
+if ((bicicleta == 'SIM') or (transporte_publico == 'SIM') or (carro_eletrico == 'SIM') or (carro == 'SIM')) and ((carona_compartilhada == 'SIM') or (caminhada == 'SIM')):
+    print('Moderada sustentabilidade para meio de transporte.')
+elif (bicicleta == 'SIM') or (transporte_publico == 'SIM') or (caminhada == 'SIM') or (carro_eletrico == 'SIM'):
+    print('Alta sustentabilidade para meio de transporte.')
+elif (carona_compartilhada == 'SIM') or (carro == 'SIM'):
+    print('Baixa sustentabilidade para meio de transporte.')
