d98a49a1528f56409ebbd297a2c1ed7766002ae9
João Pedro Panza Mainieri <joaopedro.mainieri@gmail.com>
2025-04-15 17:20:06 -0300

    Database criada
    mysql

--- Fase2/DataBase.sql
+++ Fase2/DataBase.sql
@@ -1,5 +1,18 @@
--- Active: 1744119843432@@BD-ACD@3306@BD180225117
-SHOW DATABASEs;
-CREATE TABLE usuariosPI(Id int PRIMARY KEY AUTO_INCREMENT, nome VARCHAR(100),senha VARCHAR(20));
-INSERT into usuariosPI(nome,senha) VALUES('Henrique','1234');
-CREATE Table classificação_sustentabilidade(Id_usuario int,data DATE PRIMARY KEY, sus_agua VARCHAR(50),sus_energia VARCHAR(50), sus_reciclagem VARCHAR(50), sus_transporte VARCHAR(50));
+-- Active: 1744742548804@@127.0.0.1@3306@projeto_integrador_fase2
+SHOW DATABASES;
+CREATE TABLE usuarios_PI(Id_usuario INT PRIMARY KEY AUTO_INCREMENT, nome VARCHAR(100), senha VARCHAR(20));
+INSERT INTO usuarios_PI(nome, senha) VALUES('Henrique', '1234');
+SELECT * FROM usuarios_PI;
+CREATE TABLE classificação_sustentabilidade (
+    Id_classificação INT PRIMARY KEY AUTO_INCREMENT, 
+    Id_usuario INT, 
+    data DATE, 
+    sus_agua INT, 
+    sus_energia INT, 
+    sus_reciclagem INT, 
+    sus_transporte INT
+);
+ALTER TABLE classificação_sustentabilidade RENAME COLUMN Id_classificação TO Id_classificacao;
+SELECT * FROM classificação_sustentabilidade;
+ALTER Table classificação_sustentabilidade ADD Foreign Key (Id_usuario) REFERENCES usuarios_PI(Id_usuario);
+SELECT * FROM classificação_sustentabilidade;
---
+++ Protótipos_telas_do_sistema/pTelaClassificar.py
@@ -0,0 +1,28 @@
+print("""
+-------------------------------------------------------------------------------------------
+|                        MONITORAMENTO DE SUSTENTABILIDADE PESSOAL                        |
+-------------------------------------------------------------------------------------------
+
+                                      CLASSIFICAR
+-------------------------------------------------------------------------------------------
+""")
+print("                                       DIA {data}")
+print()
+print("CLASSIFICAÇÃO DE SUSTENTABILIDADE DE ÁGUA: ")
+print("CLASSIFICAÇÃO DE SUSTENTABILIDADE DE ENERGIA: ")
+print("CLASSIFICAÇÃO DE SUSTENTABILIDADE DE RECICLAGEM: ")
+print("CLASSIFICAÇÃO DE SUSTENTABILIDADE DE MEIO DE TRANSPORTE:")
+print()
+print("                                       DIA {data}")
+print()
+print("CLASSIFICAÇÃO DE SUSTENTABILIDADE DE ÁGUA: ")
+print("CLASSIFICAÇÃO DE SUSTENTABILIDADE DE ENERGIA: ")
+print("CLASSIFICAÇÃO DE SUSTENTABILIDADE DE RECICLAGEM: ")
+print("CLASSIFICAÇÃO DE SUSTENTABILIDADE DE MEIO DE TRANSPORTE: ")
+print()
+print("                       MÉDIA DA CLASSIFICAÇÃO DE TODOS OS DIAS:")
+print()
+print("CLASSIFICAÇÃO DE SUSTENTABILIDADE DE ÁGUA: ")
+print("CLASSIFICAÇÃO DE SUSTENTABILIDADE DE ENERGIA: ")
+print("CLASSIFICAÇÃO DE SUSTENTABILIDADE DE RECICLAGEM: ")
+print("CLASSIFICAÇÃO DE SUSTENTABILIDADE DE MEIO DE TRANSPORTE: ")
