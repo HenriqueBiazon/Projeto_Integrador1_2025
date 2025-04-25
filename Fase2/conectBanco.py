1b93fb5b4bda2263b319270c704d62adaef5bd1e
João Pedro Panza Mainieri <joaopedro.mainieri@gmail.com>
2025-04-25 15:54:44 -0300

    DBalter_dia e DBdelete_dia criados

--- Fase2/conectBanco.py
+++ Fase2/conectBanco.py
@@ -12,7 +12,7 @@ def DBconnect():
     )
     return mydb
 
-def DBinsert_dados(data, consumo_agua, consumo_energia, porcentagem_reciclagem, string_meios_transporte):
+def DBinsert_dados(data, consumo_agua, consumo_energia, porcentagem_reciclagem, string_meios_transporte): #INSERE OS DADOS EM UMA NOVA LINHA DA TABELA
     mydb = DBconnect()
     cursor = mydb.cursor()
 
@@ -22,11 +22,11 @@ def DBinsert_dados(data, consumo_agua, consumo_energia, porcentagem_reciclagem, 
     cursor.execute(sql,values)
     mydb.commit() 
 
-def DBselect():
+def DBselect(): #SELECIONA A TABELA INTEIRA
     mydb = DBconnect()
     cursor = mydb.cursor()
 
-    sql = "SELECT * FROM dados_sustentabilidade"
+    sql = 'SELECT * FROM dados_sustentabilidade'
     cursor.execute(sql)
     myresult = cursor.fetchall()
 
@@ -34,3 +34,32 @@ def DBselect():
 
     return myresult
 DBconnect()
+
+def DBselect_dados(data): #SELECIONA A LINHA EM UMA DATA ESPECÍFICA
+    mydb = DBconnect()
+    cursor = mydb.cursor()
+
+    sql = f'SELECT * FROM dados_sustentabilidade WHERE data = "{data}"'
+    cursor.execute(sql)
+    myresult = cursor.fetchall()
+
+    return myresult
+
+def DBalter_dia(data, consumo_agua, consumo_energia, porcentagem_reciclagem, string_meios_transporte):
+    mydb = DBconnect()
+    cursor = mydb.cursor()
+
+    sql = 'ALTER TABLE dados_sustentabilidade (data, consumo_agua, consumo_energia, porcentagem_reciclagem, meio_transporte) VALUES (%s,%s,%s,%s,%s)'
+    values = (data, consumo_agua, consumo_energia, porcentagem_reciclagem, string_meios_transporte)
+
+    cursor.execute(sql,values)
+    mydb.commit()
+    
+def DBdelete_dia(data):
+    mydb = DBconnect()
+    cursor = mydb.cursor()
+
+    sql = f'DELETE FROM dados_sustentabilidade WHERE data = "{data}"'
+
+    cursor.execute(sql)
+    mydb.commit()
